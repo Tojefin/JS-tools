1d178837c1be03c3322ab3c7d09cf8b47552ad3b
Tojefin <tojefin@gmail.com>
2022-03-31 17:38:22 +0400

    Обновление notify

--- js/notify.js
+++ js/notify.js
@@ -1,40 +1,15 @@
 //https://github.com/Tojefin/JS-tools
-document.head.insertAdjacentHTML("beforeend", `
-  <style>
-    @keyframes notifyAnimation {
-      from {
-        opacity: 0;
-      }
 
-      to {
-        opacity: 1;
-      }
-    }
-  </style>
-`)
-
-function notifySend(content, className) {
+function notifySend(content, className, duration, closeOnClick) {
   let container = document.querySelector('body')
   let element = document.createElement('div');
   element.className = className
-  element.style.cssText = `
-    position: absolute;
-    top: 5%;
-    right: 5%;
-    background: #03adfc;
-    padding: 0 35px;
-    border-radius: 5px;
-    font-size: 10px;
-    font-hight: 12px;
-    opacity: 0;
-    transform: translateX(105%);
-    transition: all ease 0.5s;
-  `
   element.innerHTML = content
+  if (closeOnClick) {
+    element.addEventListener('click', () => {
+      container.removeChild(element)
+    })
+  }
   container.append(element)
-  setTimeout(() => {element.style.transform = 'translateX(0)'}, 100);
-  setTimeout(() => {element.style.opacity = '1'}, 100);
-  setTimeout(() => {element.style.transform = 'translateX(105%)'}, 2000);
-  setTimeout(() => {element.style.opacity = '0'}, 2000);
-  setTimeout(() => {container.removeChild(element)}, 3000);
+  setTimeout(() => {container.removeChild(element)}, duration ?? 3000);
 }
